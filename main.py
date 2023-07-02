6118e6fd7374da6b76cd6733a0c15d1b2d4bbbe6
Saiyam Jain <saiyamsandhir@gmail.com>
2023-07-02 19:26:39 +0530

    Create header label

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ class MergeXcel(ctk.CTk):
         header_frame = ctk.CTkFrame(self, corner_radius=20, border_width=5)
         header_frame.place(relx=0.5, rely=0.2, relwidth=0.95, relheight=0.3, anchor=ctk.S)
 
+        header_label = ctk.CTkLabel(header_frame, text="MergeXcel", font=("Lucida Calligraphy", 50))
+        header_label.place(relx=0.5, rely=0.65, anchor=ctk.CENTER)
+
         body_frame = ctk.CTkFrame(self, corner_radius=20, border_width=5)
         body_frame.place(relx=0.5, rely=0.225, relwidth=0.95, relheight=0.76, anchor=ctk.N)
 

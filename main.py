a2b565b1a7e3d6b32d6db64982ea64e0da49019a
Saiyam Jain <saiyamsandhir@gmail.com>
2023-07-02 18:54:35 +0530

    Create header and body frames

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ class MergeXcel(ctk.CTk):
         self.title("MergeXcel")
         self.geometry("500x600")
 
+        header_frame = ctk.CTkFrame(self, corner_radius=20, border_width=5)
+        header_frame.place(relx=0.5, rely=0.2, relwidth=0.95, relheight=0.3, anchor=ctk.S)
+
+        body_frame = ctk.CTkFrame(self, corner_radius=20, border_width=5)
+        body_frame.place(relx=0.5, rely=0.225, relwidth=0.95, relheight=0.76, anchor=ctk.N)
+
         self.mainloop()
 
 if __name__ == "__main__":

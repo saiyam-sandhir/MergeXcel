95bfa0d944e079f50101a54f0df223553fecf08a
Saiyam Jain <saiyamsandhir@gmail.com>
2023-07-04 20:55:56 +0530

    Add progress bars

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ class MergeXcel(ctk.CTk):
         logo_frame = ctk.CTkFrame(header_frame, width=400, height=100, border_width=0, fg_color="transparent")
         logo_frame.place(relx=0.5, rely=0.65, anchor=ctk.CENTER)
 
-        logo_image = ctk.CTkImage(light_image=Image.open(".\\icon.png"),
-                                  dark_image=Image.open(".\\icon.png"),
-                                  size=(100, 100))
+        logo_image = ctk.CTkImage(light_image=Image.open(".\\icon.png"),dark_image=Image.open(".\\icon.png"),size=(100, 100))
         logo_label = ctk.CTkLabel(logo_frame, image=logo_image, text="")
         logo_label.place(relx=0.15, rely=0.5, anchor=ctk.CENTER)
 
@@ -37,8 +35,12 @@ class MergeXcel(ctk.CTk):
         body_frame.place(relx=0.5, rely=0.225, relwidth=0.95, relheight=0.76, anchor=ctk.N)
 
         self.filenames = []
-        select_files_button = ctk.CTkButton(body_frame, text="SELECT", font=("Arial", 20), command=self.openFileNames)
-        select_files_button.place(relx=0.5, rely=0.1, anchor=ctk.CENTER)
+        self.select_files_button = ctk.CTkButton(body_frame, text="SELECT", font=("Arial", 20), command=self.openFileNames)
+        self.select_files_button.place(relx=0.5, rely=0.1, anchor=ctk.CENTER)
+
+        self.select_files_progress_bar = ctk.CTkProgressBar(body_frame, height=10, border_width=2, corner_radius=5, orientation="indeterminate")
+        self.select_files_progress_bar.place(relx=0.5, rely=0.1, relwidth=0.6, anchor=ctk.CENTER)
+        self.select_files_progress_bar.place_forget()
 
         self.selected_files_listbox = ctkLB.CTkListbox(body_frame)
         self.selected_files_listbox.place(relx=0.5, rely=0.45, relwidth=0.6, relheight=0.55, anchor=ctk.CENTER)
@@ -49,14 +51,22 @@ class MergeXcel(ctk.CTk):
         single_sheet_radio_button = ctk.CTkRadioButton(body_frame, text="Merge into single sheet", variable=self.radio_var, value=2)
         single_sheet_radio_button.place(relx=0.7, rely=0.8, anchor=ctk.CENTER)
 
-        merge_button = ctk.CTkButton(body_frame, text="MERGE", font=("Arial", 20), command=self.mergeFiles)
-        merge_button.place(relx=0.5, rely=0.9, anchor=ctk.CENTER)
+        self.merge_button = ctk.CTkButton(body_frame, text="MERGE", font=("Arial", 20), command=self.mergeFiles)
+        self.merge_button.place(relx=0.5, rely=0.9, anchor=ctk.CENTER)
+
+        self.merge_files_progress_bar = ctk.CTkProgressBar(body_frame, height=10, border_width=2, corner_radius=5, orientation="indeterminate")
+        self.merge_files_progress_bar.place(relx=0.5, rely=0.9, relwidth=0.6, anchor=ctk.CENTER)
+        self.merge_files_progress_bar.place_forget()
 
         self.mainloop()
-
+        
     def openFileNames(self):
         new_filenames = list(ctk.filedialog.askopenfilenames(filetypes=[("Excel files", "*.xlsx")], multiple=True))
 
+        self.select_files_button.place_forget()
+        self.select_files_progress_bar.place(relx=0.5, rely=0.1, relwidth=0.6, anchor=ctk.CENTER)
+        self.select_files_progress_bar.start()
+        
         if len(new_filenames) != 0:
             self.filenames = new_filenames
             for i in range(self.selected_files_listbox.size()):
@@ -65,14 +75,26 @@ class MergeXcel(ctk.CTk):
             for i, file in enumerate(self.filenames):
                 self.selected_files_listbox.insert(i, file)
 
+        self.select_files_progress_bar.stop()
+        self.select_files_progress_bar.place_forget()
+        self.select_files_button.place(relx=0.5, rely=0.1, anchor=ctk.CENTER)
+
     def mergeFiles(self):
         if self.radio_var.get() == 2 and len(self.filenames) != 0:
+            self.merge_button.place_forget()
+            self.merge_files_progress_bar.place(relx=0.5, rely=0.9, relwidth=0.6, anchor=ctk.CENTER)
+            self.merge_files_progress_bar.start()
+
             merged_df = pd.read_excel(self.filenames[0])
 
             for file in self.filenames[1:]:
                 df = pd.read_excel(file)
                     
                 merged_df = pd.merge(merged_df, df, how="outer")
+
+            self.merge_files_progress_bar.stop()
+            self.merge_files_progress_bar.place_forget()
+            self.merge_button.place(relx=0.5, rely=0.9, anchor=ctk.CENTER)
 
             save_loc = ctk.filedialog.asksaveasfile(filetypes=[("Excel file", "*.xlsx")], defaultextension=[("Excel file", ".xlsx")])
             merged_df.to_excel(save_loc.name, index=False)
@@ -85,7 +107,6 @@ class MergeXcel(ctk.CTk):
 
             #       writer.save()
             pass
-
 
 if __name__ == "__main__":
     MergeXcel()
